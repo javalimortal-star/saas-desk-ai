811f4f00e50fade3b1c59562e23e3a62a51f4f31
serragabriel <gabrielserralola@gmail.com>
2026-08-13 15:53:57 -0300

    feat: refresh portfolio interface (#12)

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,13 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
-    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+    "staticfiles": {
+        "BACKEND": (
+            "django.contrib.staticfiles.storage.StaticFilesStorage"
+            if DEBUG
+            else "whitenoise.storage.CompressedManifestStaticFilesStorage"
+        )
+    },
 }
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "support_requests:analyst-login"
